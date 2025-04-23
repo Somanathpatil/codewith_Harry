76aa5df333db5ebfd4b873e4ff20e43fed1272d4
Somanath patil <patilsomanath292@gmail.com>
2025-04-23 12:26:17 +0530

    this is main commit

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
 const express = require('express')
-
-const blog = require('./routes/blog')
-
-
 const app = express()
 const port = 3000
 
-app.use(express.static('public')) 
-app.use('/blog', blog)  
+app.use((req, res, next) => {
+  console.log('m1')
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log('m2')
+  next()
+})
 
 app.get('/', (req, res) => {
-  console.log("Hey its a get request")
   res.send('Hello World!')
-})
-
-app.post('/', (req, res) => {
-  console.log('Hey its a post request')
-  res.send('About Us')
-})
-
-app.get("/index",(req,res)=>{
-  console.log("hey its index")
-  res.sendFile('templates/index.html', { root: __dirname })
 })
 
 app.listen(port, () => {
